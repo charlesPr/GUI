c06e640079061962a66f6265b4ede4a7a083168e
charlesPr <46899146+charlesPr@users.noreply.github.com>
2019-02-23 21:35:16 +0400

    Add files via upload

---
+++ gui/Gui.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Pranav Seewoodharry
+ */
+public class Gui extends JFrame  {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       Online_Store CAL = new Online_Store();
+        CAL.setVisible(true);
+    }
+    
+}
